c48259ffced712d49042bf8f1d2922f690fc66a4
Sako Hartounian <sakohartounian@yahoo.com>
2018-02-18 13:09:37 -0800

    doctest added and function name changed.

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,10 +1,39 @@
 defmodule Identicon do
+  @moduledoc """
+    This is a library for creating  identicon images out of string values
+  """
+
+  @doc """
+    This is the main function for converting strings to identicons
+
+    # Examples
+      iex> Identicon.main("Banana")
+      %Identicon.Image{
+        color: {230, 249, 195},
+        grid: [
+        {230, 0},
+        {230, 4},
+        {170, 10},
+        {162, 12},
+        {170, 14},
+        {122, 16},
+        {122, 18},
+        {24, 20},
+        {244, 21},
+        {74, 22},
+        {244, 23},
+        {24, 24}
+        ],
+        hex: [230, 249, 195, 71, 103, 45, 170, 229, 162, 85, 122, 225, 24, 244, 74,
+        30]
+      }
+  """
   def main(input) do
     input
     |> hash_input
     |> pick_color
     |> build_grid
-    |> filter_odd_squares
+    |> filter_odd_squares_from_grid
   end
 
   def hash_input(input) do
@@ -32,7 +61,7 @@ defmodule Identicon do
     row ++ [second, first]
   end
 
-  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+  def filter_odd_squares_from_grid(%Identicon.Image{grid: grid} = image) do
      new_grid = Enum.filter(grid, fn({code, _}) ->
        rem(code, 2) == 0
      end)
